8a83b3f9d5f538b04dbd545c005f3fd0d01df426
reeeeedmil <agrlul@email.cz>
2024-12-15 00:32:13 +0100

    fix

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({fallback: '404.html'}),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/svelte-github-pages' : '',
+      base: process.argv.includes('dev') === 'production' ? '/MisaWeb' : '',
     },
 	},
 };
